453a73ba3b65d77cdcec2b1a9c03523c9943eae9
Yossaphol <yossaphol3502@gmail.com>
2024-07-21 15:02:59 +0700

    update

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import Head from './components/Header'
+import '../App.css'
+import Navbar from './Navbar'
+import Head from './Header'
 
 function App() {
 
